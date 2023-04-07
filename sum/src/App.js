b4745211a082b3fb792f175aff5bb2f08e250871
serayaltinalmaz <altinalmazseray@gmail.com>
2023-04-07 17:15:11 +0300

    sum

---
+++ sum/src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [numbers, setNumbers] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const inputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+  const [sumNumber, setSumNumber] = useState();
+  function numberSum(){
+    const sum = numbers.reduce((prev, current) => prev + current);
+      setSumNumber(sum);
+  };
+  function del(){
+    setNumbers([]);
+    setSumNumber();
+    setInputValue("");
+  }
+  return (
+    <div className="App">
+      <input type="number" value={inputValue} onChange={inputChange} />
+      <button onClick={() => { setNumbers([...numbers, Number((inputValue))]) }}>Ekle</button>
+      <button onClick={numberSum}>Topla</button>
+      <p>{numbers.join()}</p>
+      <p>{sumNumber}</p>
+      <button onClick={del}>Temizle</button>
+    </div>
+  );
+}
+
+export default App;
+
