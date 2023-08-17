8f5b2691dace027a535dccbe3e8544c1fa6e9f98
serayaltinalmaz <altinalmazseray@gmail.com>
2023-08-17 13:57:32 +0300

    created product sorted function

--- myapp-typescript/src/pages/Productspage.tsx
+++ myapp-typescript/src/pages/Productspage.tsx
@@ -12,6 +12,33 @@ const Productspage = () => {
     setTempProducts(products)
   }, [products])
 
+
+  function alphSorted(){
+    let alphSorted = [...products];
+    alphSorted.sort((a, b) => (a.title > b.title) ? 1 : ((b.title > a.title) ? -1 : 0));
+    console.log("A-Z", alphSorted);
+    setTempProducts(alphSorted);
+  }
+  function alphSortedReverse(){
+    let alphSorted = [...products];
+    let reverse = alphSorted.sort((a, b) => (a.title > b.title) ? 1 : ((b.title > a.title) ? -1 : 0));
+    reverse.reverse();
+    console.log("Z-A", reverse);
+    setTempProducts(reverse);
+  }
+  function priceSorted(){
+    let priceSorted = [...products];
+    priceSorted.sort((a, b) => (a.price > b.price) ? 1 : ((b.price > a.price) ? -1 : 0));
+    console.log("D-Y", priceSorted);
+    setTempProducts(priceSorted);
+  }
+  function priceSortedReverse(){
+    let priceSorted = [...products];
+    let reverse = priceSorted.sort((a, b) => (a.price > b.price) ? 1 : ((b.price > a.price) ? -1 : 0));
+    reverse.reverse();
+    console.log("Y-D", reverse);
+    setTempProducts(reverse);
+  }
   return (
     <>
       <div className="dropdown">
@@ -20,34 +47,10 @@ const Productspage = () => {
           <a onClick={() => {
             setTempProducts(products);
           }} href="#">Varsayılan</a>
-          <a onClick={() => {
-            let alphSorted = [...products];
-            //objs.sort((a,b) => (a.last_nom > b.last_nom) ? 1 : ((b.last_nom > a.last_nom) ? -1 : 0)) stackoverflowdan buldum
-            //objs.sort((a,b) => a.last_nom - b.last_nom); // b - a for reverse sort
-            alphSorted.sort((a, b) => (a.title > b.title) ? 1 : ((b.title > a.title) ? -1 : 0));
-            console.log("A-Z", alphSorted);
-            setTempProducts(alphSorted);
-          }} href="#">A-Z</a>
-          <a onClick={() => {
-            let alphSorted = [...products];
-            let reverse = alphSorted.sort((a, b) => (a.title > b.title) ? 1 : ((b.title > a.title) ? -1 : 0));
-            reverse.reverse();
-            console.log("Z-A", reverse);
-            setTempProducts(reverse);
-          }} href="#">Z-A</a>
-          <a onClick={() => {
-            let priceSorted = [...products];
-            priceSorted.sort((a, b) => (a.price > b.price) ? 1 : ((b.price > a.price) ? -1 : 0));
-            console.log("D-Y", priceSorted);
-            setTempProducts(priceSorted);
-          }} href="#">Düşükten yükseğe</a>
-          <a onClick={() => {
-            let priceSorted = [...products];
-            let reverse = priceSorted.sort((a, b) => (a.price > b.price) ? 1 : ((b.price > a.price) ? -1 : 0));
-            reverse.reverse();
-            console.log("Y-D", reverse);
-            setTempProducts(reverse);
-          }} href="#">Yüksekten düşüğe</a>
+          <a onClick={()=> alphSorted() } href="#">A-Z</a>
+          <a onClick={() => alphSortedReverse() } href="#">Z-A</a>
+          <a onClick={() => priceSorted() } href="#">Düşükten yükseğe</a>
+          <a onClick={() => priceSortedReverse() } href="#">Yüksekten düşüğe</a>
         </div>
       </div>
       <div
@@ -66,3 +69,6 @@ const Productspage = () => {
 };
 
 export default Productspage;
+
+//objs.sort((a,b) => (a.last_nom > b.last_nom) ? 1 : ((b.last_nom > a.last_nom) ? -1 : 0)) stackoverflowdan buldum
+//objs.sort((a,b) => a.last_nom - b.last_nom); // b - a for reverse sort
